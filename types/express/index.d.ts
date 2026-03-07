d3ebe77c0bcc71c2612ff175b66ac650547763ac
yadavprynce <yadavprynce@gmail.com>
2026-03-07 16:31:19 +0530

    Updated types

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,8 +1,5 @@
 import { JwtPayload } from "jsonwebtoken"
-export interface MyJwtPayload {
-  userId: string;
-  role: "USER" | "SERVICE_PROVIDER";
-}
+
 declare global{
     namespace Express {
         interface Request {
